0fe6d6054dacd233ea274c2146b4cbbdb8fec0f4
Roberto Mardeni <romarden@microsoft.com>
2020-04-07 21:12:56 +0000

    Renamed templates

--- src/Environment/Deploy-AzureResourceGroup.ps1
+++ src/Environment/Deploy-AzureResourceGroup.ps1
@@ -6,8 +6,8 @@ Param(
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = 'azuredeploy.json',
-    [string] $TemplateParametersFile = 'azuredeploy.parameters.json',
+    [string] $TemplateFile = 'azuresqldb.json',
+    [string] $TemplateParametersFile = 'azuresqldb.parameters.json',
     [string] $ArtifactStagingDirectory = '.',
     [string] $DSCSourceFolder = 'DSC',
     [switch] $ValidateOnly
